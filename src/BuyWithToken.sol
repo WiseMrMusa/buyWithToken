8a9998507590140fab1d8ac8ec88ddfb832cb73b
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-03-23 13:48:14 +0100

    Market listing and buying now added

--- src/BuyWithToken.sol
+++ src/BuyWithToken.sol
@@ -3,9 +3,11 @@ pragma solidity 0.8.18;
 
 import "./interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
-/// @title Price Feed
+/// @title Buy with Token
 /// @author Musa AbdulKareem (@WiseMrMusa)
 /// @notice This gets the exchange rate of two Tokens
 
@@ -19,14 +21,66 @@ contract BuyWithToken is Ownable {
         address contractAddress;
     }
 
+    struct Market {
+        address assetAddress;
+        uint256 assetID;
+        uint256 price;
+        string currency;
+        address seller;
+    }
+
+    // function placeBid(address _nftContractAddress, uint256 _nftTokenID) public payable {
+    //     NFTAuction storage bidNFT = nftAuctionDetails[_nftContractAddress][_nftTokenID];
+    //     require(bidNFT.nftOwner != address(0), "This NFT is not for auction");
+    //     require(bidNFT.auctionEndTime > block.timestamp, "The auction has ended");
+    //     bidNFT.bidders.push(payable(msg.sender));
+    //     Bid storage myBid = bidInformation[_nftContractAddress][_nftTokenID][msg.sender];
+    //     nftBidders[_nftContractAddress][_nftTokenID].push(msg.sender);
+    //     myBid.bidder = payable(msg.sender);
+    //     myBid.bid += msg.value;
+    //     emit BidforNFT(msg.sender);
+    // }
+
+    function listAsset(address _assetAddress, uint256 _assetID, uint256 _price) public {
+        itemID++;
+        Market storage listNFT = listedAsset[itemID];
+        listNFT.assetAddress = _assetAddress;
+        listNFT.assetID = _assetID;
+        listNFT.price = _price;
+        listNFT.currency = "ETH";
+        listNFT.seller = msg.sender;
+        IERC721(_assetAddress).approve(address(this),_assetID);
+    }
+    function listAsset(address _assetAddress, uint256 _assetID, uint256 _price, string calldata _symbol) public {
+        itemID++;
+        Market storage listNFT = listedAsset[itemID];
+        listNFT.assetAddress = _assetAddress;
+        listNFT.assetID = _assetID;
+        listNFT.price = _price;
+        listNFT.currency = _symbol;
+        listNFT.seller = msg.sender;
+        IERC721(_assetAddress).approve(address(this),_assetID);
+    }
+
+    function buyAssetWithToken(uint256 itemMarketID) public {
+        Market memory item = listedAsset[itemMarketID];
+        IERC20(tokenDetails["USDT"].contractAddress).transferFrom(msg.sender,item.seller,uint256(getSwapTokenPrice(item.currency,"USDT",int256(item.price))));
+    }
+    function buyAssetWithToken(uint256 itemMarketID, string memory _token) public {
+        Market memory item = listedAsset[itemMarketID];
+        IERC20(tokenDetails["USDT"].contractAddress).transferFrom(msg.sender,item.seller,uint256(getSwapTokenPrice(item.currency,_token,int256(item.price))));
+    }
+
     /// @dev This maps the token address to the aggregator's address
+    uint256 itemID;
     mapping (string => address) private aggregrator;
     mapping (string => Token) private tokenDetails;
+    mapping (uint256 => Market) private listedAsset;
 
     constructor(){
         addTokenDetails(
             "Ethereum","ETH",18,
-            0xAc559F25B1619171CbC396a50854A3240b6A4e99,
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419,
             0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
         );
         addTokenDetails(
@@ -72,6 +126,28 @@ contract BuyWithToken is Ownable {
     /// @param _to This is the token you are swapping to
     /// @param _decimals This is the decimal of the token you are swapping to
     function getDerivedPrice(
+        string memory _from,
+        string memory _to,
+        uint8 _decimals
+    ) public view returns (int256) {
+        require(
+            _decimals > uint8(0) && _decimals <= uint8(18),
+            "Invalid _decimals"
+        );
+        int256 decimals = int256(10 ** uint256(_decimals));
+        (, int256 fromPrice, , , ) = AggregatorV3Interface(tokenDetails[_from].aggregratorAddress)
+            .latestRoundData();
+        uint8 fromDecimals = AggregatorV3Interface(tokenDetails[_from].aggregratorAddress).decimals();
+        fromPrice = scalePrice(fromPrice , fromDecimals, _decimals);
+
+        (, int256 toPrice, , , ) = AggregatorV3Interface(tokenDetails[_to].aggregratorAddress)
+            .latestRoundData();
+        uint8 toDecimals = AggregatorV3Interface(tokenDetails[_to].aggregratorAddress).decimals();
+        toPrice = scalePrice(toPrice, toDecimals, _decimals);
+
+        return (fromPrice * decimals) / toPrice;
+    }
+    function getDerivedPriceBase(
         string calldata _from,
         string calldata _to,
         uint8 _decimals
@@ -94,6 +170,19 @@ contract BuyWithToken is Ownable {
         return (fromPrice * decimals) / toPrice;
     }
 
+    function factorPrice(
+        int256 _price,
+        uint8 _priceDecimals,
+        uint8 _decimals
+    ) internal pure returns (int256) {
+        if (_priceDecimals < _decimals) {
+            return _price * int256(10 ** uint256(_decimals - _priceDecimals));
+        } else if (_priceDecimals > _decimals) {
+            return _price / int256(10 ** uint256(_priceDecimals - _decimals));
+        }
+        return _price;
+    }
+
     function scalePrice(
         int256 _price,
         uint8 _priceDecimals,
@@ -108,11 +197,11 @@ contract BuyWithToken is Ownable {
     }
 
     function getSwapTokenPrice(
-        string calldata _fromToken, 
-        string calldata _toToken,
+        string memory _fromToken, 
+        string memory _toToken,
         // uint8 _decimals,
         int256 _amount
-    ) external view returns (int256) {
+    ) public view returns (int256) {
         return _amount * getDerivedPrice(
             _fromToken,
              _toToken,
--- test/BuyWithToken.t.sol
+++ test/BuyWithToken.t.sol
@@ -5,33 +5,26 @@ import "forge-std/Test.sol";
 import "../src/BuyWithToken.sol";
 
 contract BuyWithTokenTest is Test {
-    BuyWithToken buy;
+    BuyWithToken market;
 
     function setUp() public {
-        uint256 mainnet = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/Bpgb9sxlII8NJjFOZCcMlnqzsK5dKk0L", 15_171_037);
+        uint256 mainnet = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/Bpgb9sxlII8NJjFOZCcMlnqzsK5dKk0L", 16_889_761);
         vm.selectFork(mainnet);
-
-        buy = new BuyWithToken();
-        // buy.addTokenDetails("BTC",8, 0xA39434A63A52E749F02807ae27335515BA4b07F7,address(0));
-        // // buy.addTokenDetails("CZK",0xAE45DCb3eB59E27f05C170752B218C6174394Df8);
-        // // buy.addTokenDetails("DAI",0x0d79df66BE487753B02D015Fb622DED7f0E9798d);
-        // buy.addTokenDetails("ETH",18,0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e, address(0));
-        // buy.addTokenDetails("EUR",0x44390589104C9164407A0E0562a9DBe6C24A0E05);
-        // buy.addTokenDetails("FORTH",0x7A65Cf6C2ACE993f09231EC1Ea7363fb29C13f2F);
-        // buy.addTokenDetails("GBP",0x73D9c953DaaB1c829D01E1FC0bd92e28ECfB66DB);
-        // buy.addTokenDetails("JPY",0x982B232303af1EFfB49939b81AD6866B2E4eeD0B);
-        // buy.addTokenDetails("LINK",0x48731cF7e84dc94C5f84577882c14Be11a5B7456);
-        // buy.addTokenDetails("USDC",0xAb5c49580294Aff77670F839ea425f5b78ab3Ae7);
-        // buy.addTokenDetails("XAU",0x7b219F57a8e9C7303204Af681e9fA69d17ef626f);
+        market = new BuyWithToken();
     }
 
     function testExchange() public view {
-        buy.getSwapTokenPrice("ETH","USDT",2);
+        market.getSwapTokenPrice("ETH","USDT",1);
+        // console.log(us);
         // buy.getSwapTokenPrice("DAI","GBP",8,500);
         // buy.getSwapTokenPrice("FORTH","LINK",8,200);
         // buy.getSwapTokenPrice("CZK","BTC",8,1000);
         // buy.getSwapTokenPrice("JPY","EUR",8,75);
         // buy.getSwapTokenPrice("ETH","USDC",8,3.2 ether);
+
+        // 614_880_141_589_000_000_000_000_000_000
+        // 1756.80_040_454
+        // 1756.80_040_454
     }
 
 }
